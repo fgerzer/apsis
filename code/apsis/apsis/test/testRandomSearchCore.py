527a287d1999329421848d468fff8e9569bb94c6
Frederik Diehl <frederikdiehl@gmail.com>
2014-10-10 10:58:05 +0200

    First nose test. Now with file.

---
+++ code/apsis/apsis/test/testRandomSearchCore.py
@@ -0,0 +1,17 @@
+from apsis.RandomSearchCore import RandomSearchCore
+import numpy as np
+
+
+
+lower_bound = np.zeros((3,1))
+upper_bound = np.ones((3,1))
+
+def setup_func():
+    "set up test fixtures"
+
+def teardown_func():
+    "tear down test fixtures"
+
+@with_setup(setup_func, teardown_func)
+def test_initialization():
+    assert RandomSearchCore(lower_bound, upper_bound) is not None
