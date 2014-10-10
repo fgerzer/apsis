2c5ce6a4bd643c9ceb034e7d623e6376466449bf
Frederik Diehl <frederikdiehl@gmail.com>
2014-10-10 10:57:26 +0200

    First nose test.

--- code/apsis/apsis/test/RandomSearchCoreTester.py
+++
@@ -1,8 +0,0 @@
-from apsis.RandomSearchCore import RandomSearchCore
-import numpy as np
-
-lower_bound = np.zeros((3,1))
-upper_bound = np.ones((3,1))
-
-randomSearchCore = RandomSearchCore(lower_bound, upper_bound)
-
