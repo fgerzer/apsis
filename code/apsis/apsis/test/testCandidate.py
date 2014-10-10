e07c8826661be3c41e55ffe399857591f8da6f5e
Frederik Diehl <frederikdiehl@gmail.com>
2014-10-10 11:41:08 +0200

    Added tests for the Candidate class.

---
+++ code/apsis/apsis/test/testCandidate.py
@@ -0,0 +1,23 @@
+__author__ = 'Frederik Diehl'
+
+from apsis.Candidate import Candidate
+import numpy as np
+import random
+
+class testCandidate():
+
+    test_point = None
+    test_candidate = None
+
+
+
+    def setUp(self):
+        self.test_point = np.ones((3, 1))
+        for i in range(0, self.test_point.shape[0]):
+            self.test_point[i] = random.gauss(0, 10)
+        self.test_candidate = Candidate(self.test_point)
+
+    def test_initialization(self):
+        assert self.test_candidate is not None
+        assert isinstance(self.test_candidate, Candidate)
+        assert (self.test_candidate.params == self.test_point).all
