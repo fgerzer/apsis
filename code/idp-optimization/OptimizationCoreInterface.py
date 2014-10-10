e1e4569a19beeb25f1b27bc6c9790538e27ad635
Andreas Jauch <AJAUCH@de.ibm.com>
2014-10-10 10:01:13 +0200

    changed optmizationCoreInterface to have a mandatory constructer with upper and lower bound for optimization space

--- code/idp-optimization/OptimizationCoreInterface.py
+++ code/idp-optimization/OptimizationCoreInterface.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python
 from abc import ABCMeta, abstractmethod
 
+
 class OptimizationCoreInterface:
     __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __init__(self, params_lower_bound, params_upper_bound):
+        """
+        Init method to initialize optimization core.
+        :param params_lower_bound: Lower bound of optimization search space. Expected is a numpy vector of floats,
+        one per dimension of the parameter space.
+        :param params_upper_bound: Upper bound of optimization search space. Expected is a numpy vector of floats,
+        one per dimension of the parameter space.
+        :return: this object.
+        """
 
     @abstractmethod
     def next_candidate(self, worker_id=None):
@@ -15,14 +27,16 @@ class OptimizationCoreInterface:
 
 
     @abstractmethod
-    def working(self, candidate, status, can_be_killed=False):
+    def working(self, candidate, status, worker_id=None, can_be_killed=False):
         """
         Method that is used by used by workers to
-        :param candidate: an object of type Candidate that contains information related to the point the worker is currently evaluating
+        :param candidate: an object of type Candidate that contains information related to the point the worker is
+        currently evaluating
         :param status: a string containing either of the values ('finished', 'working', 'paused').
-        A worker sending 'finished' indicates that it stops the evaluation. Sending 'working' indicates that the worker wants to go on working
-        on this point. 'paused' indicates that the worker needed to pause for some external reason and continuing on evaluating this point
-        should be done by another worker.
+        A worker sending 'finished' indicates that it stops the evaluation. Sending 'working' indicates that the worker
+        wants to go on working on this point. 'paused' indicates that the worker needed to pause for some external
+        reason and continuing on evaluating this point should be done by another worker.
+        :param worker_id: A string id for the worker calling this method. Unused by default.
         :param can_be_killed: A boolean stating if the worker can be killed from the core or not.
         :return: a boolean to tell the worker if it should continue or stop the evaluation
         """
