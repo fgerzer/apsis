f688cda1aacd7cefdf6029e614fc00585257a311
Andreas Jauch <AJAUCH@de.ibm.com>
2014-10-10 11:13:26 +0200

    work on random search core

--- code/apsis/apsis/RandomSearchCore.py
+++ code/apsis/apsis/RandomSearchCore.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python
 
 from apsis.OptimizationCoreInterface import OptimizationCoreInterface
+from sklearn.utils import check_random_state
+import numpy as np
 
 
 class RandomSearchCore(OptimizationCoreInterface):
-    def __init__(self, lower_bound, upper_bound):
+    lower_bound = None
+    upper_bound = None
+    random_state = None
+
+    def __init__(self, lower_bound, upper_bound, random_state=0):
         print("Initializing Random Search Core for bounds..." + str(lower_bound) + " and " + str(upper_bound))
 
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.random_state = check_random_state(random_state)
+
     def working(self, candidate, status, worker_id=None, can_be_killed=False):
-        print("Worker seinding worker informatiojn")
+        print("Worker sending worker information")
 
     def next_candidate(self, worker_id=None):
-        return None
+        new_candidate = np.zeros(self.lower_bound.shape)
+
+        for i in range(new_candidate.shape[0]):
+            new_candidate[i] = self.random_state.uniform(self.lower_bound[i], self.upper_bound[i])
+
+        return new_candidate
 
--- code/apsis/apsis/test/testRandomSearchCore.py
+++ code/apsis/apsis/test/testRandomSearchCore.py
@@ -3,8 +3,7 @@ import numpy as np
 
 from nose import with_setup
 
-lower_bound = np.zeros((3,1))
-upper_bound = np.ones((3,1))
+random_search_core = None
 
 def setup_func():
     "set up test fixtures"
@@ -12,6 +11,20 @@ def setup_func():
 def teardown_func():
     "tear down test fixtures"
 
-@with_setup(setup_func, teardown_func)
+def random_init():
+    lower_bound = np.zeros((3,1))
+    upper_bound = np.ones((3,1))
+    global random_search_core
+    random_search_core = RandomSearchCore(lower_bound, upper_bound)
+
+
 def test_initialization():
+    lower_bound = np.zeros((3,1))
+    upper_bound = np.ones((3,1))
     assert RandomSearchCore(lower_bound, upper_bound) is not None
+
+@with_setup(random_init)
+def test_next_candidate():
+    next_candidate = random_search_core.next_candidate()
+
+    print(str(next_candidate))
