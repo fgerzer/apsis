6b4cb4b80f77fe52d00314624c3b0283b3efa07e
Frederik Diehl <frederikdiehl@gmail.com>
2014-10-24 17:34:36 +0200

    Currently working for simple test case and bad optimization. Further work required.

--- code/apsis/apsis/bayesian/AcquisitionFunctions.py
+++ code/apsis/apsis/bayesian/AcquisitionFunctions.py
@@ -3,7 +3,7 @@ import numpy as np
 import scipy.optimize
 import logging
 from scipy.integrate import quad
-
+from scipy.stats import multivariate_normal
 
 class AcquisitionFunction(object):
     __metaclass__ = ABCMeta
@@ -19,18 +19,89 @@ class AcquisitionFunction(object):
         pass
 
     @abstractmethod
-    def evaluate(self, args):
-        if args is None:
+    def evaluate(self, x, args_):
+        if args_ is None:
             raise ValueError("No arguments dict given!")
 
         pass
 
     @abstractmethod
-    def compute_max(self, args):
-        if args is None:
+    def compute_max(self, args_):
+        if args_ is None:
             raise ValueError("No arguments dict given!")
 
         pass
+
+class ExpectedImprovement(AcquisitionFunction):
+    def __init__(self, params=None):
+        super(ExpectedImprovement, self).__init__(params=None)
+
+    def compute_max(self, args_):
+        dimensions = len(args_['param_defs'])
+
+        logging.debug("dimensions of param defs %s", str(dimensions))
+
+        initial_guess = [0.5] * dimensions
+        initial_guess = tuple(initial_guess)
+
+        bounds = tuple([(0, 1) * dimensions])
+
+        logging.debug("Random guess %s length %s, bounds %s, length %s",
+                      str(initial_guess), str(len(initial_guess)), str(bounds),
+                      str(len(bounds)))
+
+        # use scipy.optimize.minimize,
+        # make sure to use minimizing value from the result object
+        #minimum = scipy.optimize.minimize(self.compute_negated_evaluate,
+        #                                  initial_guess, args=tuple([args_]),
+        #                                  bounds=bounds, method="SLSQP").x
+        cur = np.zeros((1, 1))
+        minimum = np.zeros((1, 1))
+        for i in range(1000):
+            cur[0, 0] += 1./1000
+            if (self.evaluate(cur, args_))[0, 0] > self.evaluate(minimum, args_)[0, 0]:
+                #print("new max: %s, %s" %(str(self.evaluate(cur, args_)), str(cur)))
+                minimum[0, 0] = cur[0, 0]
+                #print("new max: %s, %s" %(str(self.evaluate(minimum, args_)), str(minimum)))
+
+        print("min. " + str(minimum))
+        return minimum
+
+    def compute_negated_evaluate(self, x, args_):
+        value = self.evaluate(x, args_)
+        value = -value
+
+        return value
+
+
+
+    def evaluate(self, x, args_):
+        cur_format = np.zeros((1, 1))
+        cur_format[0, 0] = x
+        #TODO !!!! Only for testing!!!
+        mean, variance, _025pm, _975pm = args_['gp'].predict(cur_format)
+
+        #logging.debug("Evaluating GP mean %s, var %s, _025 %s, _975 %s", str(mean),
+        #              str(variance), str(_025pm), str(_975pm))
+
+        # do not standardize on our own, but use the mean, and covariance
+        #we get from the gp
+        #pdf = scipy.stats.multivariate_normal.pdf
+        #cdf_calculate = quad(pdf, 0, x, (mean[0, :], variance))
+
+
+        #return cdf_calculate[0] * max(0, mean - args_["cur_max"])
+        #TODO scale is stand dev, variance is var. Need to scale.
+        #return 1 - scipy.stats.norm(loc=mean, scale=variance).cdf(args_["cur_max"])# *max(0, mean - args_["cur_max"])
+
+        Z = (mean - args_["cur_max"])/variance
+        cdfZ = 1 - scipy.stats.norm(loc=mean, scale=variance).cdf(Z)# *max(0, mean - args_["cur_max"])
+        pdfZ = 1 - scipy.stats.norm(loc=mean, scale=variance).pdf(Z)# *max(0, mean - args_["cur_max"])
+        if variance != 0:
+            return (mean - args_["cur_max"])*cdfZ + variance*pdfZ
+        else:
+            return 0
+
 
 
 class ProbabilityOfImprovement(AcquisitionFunction):
@@ -39,13 +110,14 @@ class ProbabilityOfImprovement(AcquisitionFunction):
 
         mean, variance, _025pm, _975pm = args_['gp'].predict(x)
 
-        logging.debug("GP mean %s, var %s, _025 %s, _975 %s", str(mean),
+        logging.debug("Evaluating GP mean %s, var %s, _025 %s, _975 %s", str(mean),
                       str(variance), str(_025pm), str(_975pm))
 
         # do not standardize on our own, but use the mean, and covariance
         #we get from the gp
-        cdf_calculate = quad(scipy.stats.multivariate_normal.pdf, 0, x,
-                             (mean[0, :], variance))
+        #TODO only one dimensional - don't need multivariate
+        pdf = scipy.stats.multivariate_normal.pdf
+        cdf_calculate = quad(pdf, 0, x, (mean[0, :], variance))
 
         return cdf_calculate[0]
 
@@ -63,13 +135,11 @@ class ProbabilityOfImprovement(AcquisitionFunction):
                       str(initial_guess), str(len(initial_guess)), str(bounds),
                       str(len(bounds)))
 
-        """
-        use scipy.optimize.minimize,
-        make sure to use minimizing value from the result object
-        """
+        # use scipy.optimize.minimize,
+        # make sure to use minimizing value from the result object
         minimum = scipy.optimize.minimize(self.compute_negated_evaluate,
                                           initial_guess, args=tuple([args_]),
-                                          method=None, bounds=bounds).x
+                                          bounds=bounds, method="Anneal").x
 
         return minimum
 
--- code/apsis/apsis/test/testSimpleBayesianOptimizationCore.py
+++ code/apsis/apsis/test/testSimpleBayesianOptimizationCore.py
@@ -4,26 +4,136 @@ from apsis.adapters.SimpleScikitLearnAdapter import SimpleScikitLearnAdapter
 from sklearn.metrics import mean_squared_error
 from apsis.models.ParamInformation import NumericParamDef, NominalParamDef, \
     LowerUpperNumericParamDef
-
+from apsis.SimpleBayesianOptimizationCore import SimpleBayesianOptimizationCore
+import math
+import nose.tools as nt
+import random
+import logging
+import matplotlib.pyplot as plt
+import numpy as np
 
 class testSimpleBayesianOptimizationCore(object):
 
-    def test_setup(self):
-        boston_data = datasets.load_boston()
-        regressor = LogisticRegression()
+    # def test_setup(self):
+    #     logging.basicConfig(level=logging.DEBUG)
+    #     boston_data = datasets.load_boston()
+    #     regressor = LogisticRegression()
+    #
+    #     param_defs = {
+    #         "C": LowerUpperNumericParamDef(0, 1)
+    #     }
+    #
+    #     sk_adapter = SimpleScikitLearnAdapter(regressor, param_defs,
+    #                                           scoring="mean_squared_error",
+    #                                           n_iter=10,
+    #                                           optimizer='SimpleBayesianOptimizationCore',
+    #                                           optimizer_arguments={'initial_random_runs': 3}
+    #                                           )
+    #
+    #     fitted = sk_adapter.fit(boston_data.data, boston_data.target)
+    #
+    #     print(mean_squared_error(boston_data.target,
+    #                              fitted.predict(boston_data.data)))
 
-        param_defs = {
-            "C": LowerUpperNumericParamDef(0, 1)
-        }
+    def test_convergence_one_worker(self):
+        min_val = 0
+        max_val = 10
+        resolution = 1000
 
-        sk_adapter = SimpleScikitLearnAdapter(regressor, param_defs,
-                                              scoring="mean_squared_error",
-                                              n_iter=20,
-                                              optimizer='SimpleBayesianOptimizationCore',
-                                              optimizer_arguments={'initial_random_runs': 3}
-                                              )
+        logging.basicConfig(level=logging.DEBUG)
+        self.bay_search = SimpleBayesianOptimizationCore({"param_defs":
+            [LowerUpperNumericParamDef(min_val, max_val)],
+            "initial_random_runs": 5, 'num_gp_restarts': 4})
+        strings = []
+        f = function
+        best_result = None
+        for i in range(10):
+            cand = self.bay_search.next_candidate()
+            point = cand.params
+            value = f(point[0])
+            if (i >= 5):
+                self.plot_nicely(min_val, max_val, resolution, point[0])
+                raw_input()
+            strings.append(("%i: %f at %f" % (i, value, point[0])))
+            if best_result is None or value < best_result:
+                best_result = value
 
-        fitted = sk_adapter.fit(boston_data.data, boston_data.target)
+            cand.result = value
+            assert not self.bay_search.working(cand, "finished")
 
-        print(mean_squared_error(boston_data.target,
-                                 fitted.predict(boston_data.data)))
+        nt.eq_(self.bay_search.best_candidate.result, best_result,
+                       str(self.bay_search.best_candidate.result)
+                       + " != " + str(best_result))
+        #self.bay_search.gp.plot()
+        self.plot_nicely(min_val, max_val, resolution)
+        for s in strings:
+            print(s)
+        raw_input()
+
+
+    def plot_nicely(self, min_val, max_val, resolution, next_pt=None):
+        step_res = (max_val - min_val)/float(resolution)
+        gp_mean = []
+        gp_975 = []
+        gp_025 = []
+        acq = []
+        func = []
+        axis = []
+        plt.figure()
+
+        cur = float(min_val)
+        warped_cur = 0
+        for s in range(resolution):
+            axis.append(cur)
+
+            cur_format = np.zeros((1, 1))
+            cur_format[0, 0] = warped_cur
+            mean, variance, _025pm, _975pm = self.bay_search.gp.predict(cur_format)
+            gp_mean.append(mean)
+            gp_975.append(_975pm)
+            gp_025.append(_025pm)
+            acquisition_params = {
+                'param_defs': self.bay_search.param_defs,
+                'gp': self.bay_search.gp,
+                'cur_max': self.bay_search.best_candidate.result
+                }
+            eval = self.bay_search.acquisition_function.evaluate(warped_cur, acquisition_params)
+            acq.append(eval)
+            func.append(function(cur))
+            cur += step_res
+            warped_cur += 1./resolution
+
+        acq_scale = 1
+        max_acq = max([X[0, 0] for X in acq])
+        max_gp = float(max([X[0, 0] for X in gp_mean]))
+        max_func = float(max(func))
+        acq_scale = max_acq / max(max_gp, max_func)
+        do_scale = False
+        if do_scale:
+            acq = [X[0, 0]/acq_scale for X in acq]
+        else:
+            acq = [X[0, 0] for X in acq]
+        plt.plot(axis, [X[0, 0] for X in gp_mean], color="blue")
+        plt.plot(axis, [X[0, 0] for X in gp_975], color="green")
+        plt.plot(axis, [X[0, 0] for X in gp_025], color="green")
+        plt.plot(axis, acq, color="red")
+
+        pts = []
+        vals = []
+        for c in self.bay_search.finished_candidates:
+            pts.append(c.params[0])
+            vals.append(c.result)
+
+        plt.scatter(pts, vals)
+
+        if (next_pt is not None):
+            plt.scatter([next_pt], [function(next_pt)], color="red")
+            print("Next pt: %s, for %f acq value" %(str(next_pt), self.bay_search.acquisition_function.evaluate((next_pt-min_val)/(max_val-min_val), acquisition_params)))
+            print("Comp pt: %s, for %f acq value" %(str(0.01), self.bay_search.acquisition_function.evaluate((0.01-min_val)/(max_val-min_val), acquisition_params)))
+        else:
+            print("Next pt is None.")
+        plt.plot(axis, func, color="yellow")
+        plt.show()
+
+def function(x):
+    return math.sin(x) * x**2
