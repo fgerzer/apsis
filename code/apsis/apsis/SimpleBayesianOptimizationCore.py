a9793a1a5af99273405c02e0bf6c2e0b07dbb2a8
Frederik Diehl <frederikdiehl@gmail.com>
2014-11-09 17:44:40 +0100

    Fixes #51. SimpleBayesianOptimizationCore documentation and code cleanup.

--- code/apsis/apsis/SimpleBayesianOptimizationCore.py
+++ code/apsis/apsis/SimpleBayesianOptimizationCore.py
@@ -1,14 +1,21 @@
-from apsis.OptimizationCoreInterface import OptimizationCoreInterface, ListBasedCore
+from apsis.OptimizationCoreInterface import OptimizationCoreInterface, \
+    ListBasedCore
 from apsis.RandomSearchCore import RandomSearchCore
 from apsis.models.Candidate import Candidate
 from apsis.models.ParamInformation import NumericParamDef
-from apsis.bayesian.AcquisitionFunctions import ProbabilityOfImprovement, ExpectedImprovement
+from apsis.bayesian.AcquisitionFunctions import ExpectedImprovement
 import numpy as np
 import GPy
 import logging
 
 
 class SimpleBayesianOptimizationCore(OptimizationCoreInterface, ListBasedCore):
+    """
+    This implements a simple bayesian optimizer.
+
+    It is simple because it only implements the simplest form - no freeze-thaw,
+    (currently) now multiple workers, only numeric parameters.
+    """
     SUPPORTED_PARAM_TYPES = [NumericParamDef]
 
     kernel = None
@@ -37,7 +44,6 @@ class SimpleBayesianOptimizationCore(OptimizationCoreInterface, ListBasedCore):
             # invoke the refitting
             if len(self.finished_candidates) >= self.initial_random_runs:
                 self._refit_gp()
-
             return False
 
         elif status == "working":
@@ -58,47 +64,38 @@ class SimpleBayesianOptimizationCore(OptimizationCoreInterface, ListBasedCore):
         return True
 
     def __init__(self, params):
+        super(SimpleBayesianOptimizationCore, self).__init__(params)
+
         if params.get('param_defs', None) is None:
             raise ValueError("Parameter definition list is missing!")
-
-        # check if param_defs are supported
         if not self._is_all_supported_param_types(params["param_defs"]):
             raise ValueError(
                 "Param list contains parameters of unsopported types. "
                 "Supported types are  " + str(self.SUPPORTED_PARAM_TYPES))
 
         self.param_defs = params["param_defs"]
-
         self.minimization = params.get('minimization', True)
-
         self.initial_random_runs = params.get('initial_random_runs',
                                               self.initial_random_runs)
         self.random_state = params.get('random_state', None)
         self.finished_candidates = []
         self.working_candidates = []
         self.pending_candidates = []
-
-        self.acquisition_hyperparams = params.get('acquisition_hyperparams', None)
-
-        # either we have a given acquisition func or we use the standard one
+        self.acquisition_hyperparams = params.get('acquisition_hyperparams',
+                                                  None)
         self.acquisition_function = params.get('acquisition',
-                                               ExpectedImprovement)(self.acquisition_hyperparams)
-
+                                               ExpectedImprovement)(
+            self.acquisition_hyperparams)
         self.num_gp_restarts = params.get('num_gp_restarts',
                                           self.num_gp_restarts)
 
-        # same for kernel
         dimensions = len(self.param_defs)
-        logging.debug(dimensions)
         self.kernel = params.get('kernel',
                                  GPy.kern.rbf)(dimensions, ARD=True)
-
         logging.debug("Kernel input dim " + str(self.kernel.input_dim))
         logging.debug("Kernel %s", str(self.kernel))
-
-        #generate random samples
         random_searcher = RandomSearchCore({'param_defs': self.param_defs,
-            "random_state": self.random_state})
+                                            "random_state": self.random_state})
         for i in range(self.initial_random_runs):
             self.pending_candidates.append(random_searcher.next_candidate())
 
@@ -116,9 +113,10 @@ class SimpleBayesianOptimizationCore(OptimizationCoreInterface, ListBasedCore):
                                   'gp': self.gp,
                                   'cur_max': self.best_candidate.result,
                                   "minimization": self.minimization
-                                  }
+            }
 
-            logging.debug("Running acquisition with args %s", str(acquisition_params))
+            logging.debug("Running acquisition with args %s",
+                          str(acquisition_params))
             new_candidate_point = self.acquisition_function.compute_max(
                 acquisition_params)
 
@@ -150,25 +148,25 @@ class SimpleBayesianOptimizationCore(OptimizationCoreInterface, ListBasedCore):
 
         self.gp = GPy.models.GPRegression(candidate_matrix, results_vector,
                                           self.kernel)
-        self.gp.constrain_bounded('.*lengthscale*',0.1,1.)
-        self.gp.constrain_bounded('.*noise*',0.1,1.)
+        self.gp.constrain_bounded('.*lengthscale*', 0.1, 1.)
+        self.gp.constrain_bounded('.*noise*', 0.1, 1.)
         logging.debug("Generated gp model. Refitting now.")
-        #ensure restart
+        # ensure restart
         self.gp.optimize_restarts(num_restarts=self.num_gp_restarts,
                                   verbose=False)
-        #self.gp.optimize()
-
-
         logging.debug("Finished generating model.")
 
-
     def _warp_in(self, param_vector):
+        """
+        Warps the parameter vector, using the warp_in function for each
+        self.param_defs.
+        """
         logging.debug("Param_vector: %s" % str(param_vector))
         for i in range(len(param_vector)):
-            logging.debug("Warping in. %f to %f" %(param_vector[i],
-               self.param_defs[i].warp_in(param_vector[i])))
+            logging.debug("Warping in. %f to %f" % (param_vector[i],
+                                self.param_defs[i].warp_in(param_vector[i])))
             param_vector[i] = self.param_defs[i].warp_in(
                 param_vector[i]
-        )
-        logging.debug("New param_vector: %s" %str(param_vector))
+            )
+        logging.debug("New param_vector: %s" % str(param_vector))
         return param_vector
