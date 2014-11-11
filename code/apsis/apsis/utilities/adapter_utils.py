f290534b3a32b6c37766b0c1d3fe3e47430dab94
Frederik Diehl <frederikdiehl@gmail.com>
2014-11-11 12:28:35 +0100

    Fixes #46. adapter_utils: Docu and cleanup.

--- code/apsis/apsis/utilities/adapter_utils.py
+++ code/apsis/apsis/utilities/adapter_utils.py
@@ -12,19 +12,31 @@ def check_optimizer(optimizer):
     The argument will be checked on whether it is an instance of
     OptimizerCoreInterface (in which case it will be returned) or a string
     representing one, in which case an Instance of it will be returned.
-    :param optimizer: The optimizer or string to check.
-    :return: An OptimizationCoreInterface fulfilling instance.
-    :raise ValueError: If optimizer is not a subclass of
-    OptimizationCoreInterface or a string representing one.
+
+    Parameters
+    ----------
+
+    optimizer: string or OptimizationCoreInterface
+        The optimizer or string to check.
+
+    Returns
+    -------
+
+    optimizer: OptimizationCoreInterface
+        An OptimizationCoreInterface fulfilling instance.
+
+    Raises
+    ------
+    ValueError:
+        If optimizer is not a subclass of OptimizationCoreInterface or a string
+        representing one.
     """
     if isinstance(optimizer, OptimizationCoreInterface):
         return optimizer
 
-    print(optimizers_available[0].__name__)
     index = [X.__name__ for X in optimizers_available].index(optimizer)
 
     if index != -1:
         return optimizers_available[index]
     else:
-        raise ValueError("Optimizer " + optimizer + "not found")
-
+        raise ValueError("Optimizer " + optimizer + "not found.")
