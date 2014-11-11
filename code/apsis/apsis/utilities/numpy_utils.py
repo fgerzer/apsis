f1a70fb0fbc5600aba637dece813663afb578b86
Frederik Diehl <frederikdiehl@gmail.com>
2014-11-11 12:25:25 +0100

    Fixes #47. numpy_utils: docu and cleanup.

--- code/apsis/apsis/utilities/numpy_utils.py
+++ code/apsis/apsis/utilities/numpy_utils.py
@@ -1,8 +1,21 @@
 import numpy as np
 
-def ensure_np_array(input):
-    #a bit hacky, but for 1d check if array, if not make one manually
-    if not isinstance(input, np.ndarray):
-        input = np.asarray([input])
 
-    return input
+def ensure_np_array(array_in):
+    """
+    This function ensures the input is present as a numpy array.
+
+    Parameters
+    ----------
+    input: list or np array
+        The array to be converted.
+
+    Returns
+    -------
+    output: np.ndarray
+        The unchanged input iff it was an np.ndarray, the converted input
+        otherwise.
+    """
+    if not isinstance(array_in, np.ndarray):
+        array_in = np.asarray([array_in])
+    return array_in
