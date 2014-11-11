8eb0489ab5a0170834336a5c85e4113e8574ad7b
Frederik Diehl <frederikdiehl@gmail.com>
2014-11-11 14:29:56 +0100

    Fixes #40. param_defs: Docu and cleanup.

--- code/apsis/apsis/models/ParamInformation.py
+++ code/apsis/apsis/models/ParamInformation.py
@@ -1,24 +1,42 @@
 from abc import ABCMeta, abstractmethod
 import random
 
-
-"""
-Base Classes for Param Definition Classes
-"""
-
-
 class ParamDef(object):
-    __metaclass__ = ABCMeta
+    """
+    This represents the base class for a parameter definition.
 
-    def __init__(self):
-        pass
+    Every member of this class has to implement at least the
+    is_in_parameter_domain method to check whether objects are in the parameter
+    domain.
+    """
+    __metaclass__ = ABCMeta
 
     @abstractmethod
     def is_in_parameter_domain(self, value):
+        """
+        Should test whether a certain value is in the parameter domain as
+        defined by this class.
+
+        Parameters
+        ----------
+        value: object
+            Tests whether the object is in the parameter domain.
+
+        Returns
+        -------
+        is_in_parameter_domain: bool
+            True iff value is in the parameter domain as defined by this
+            instance.
+        """
         pass
 
 
 class ComparableParameterDef(object):
+    """
+    This class defines an ordinal parameter definition subclass, that is a
+     parameter definition in which the values are comparable.
+    It additionally implements the compare_values_function.
+    """
     __metaclass__ = ABCMeta
 
     def __init__(self):
@@ -27,24 +45,54 @@ class ComparableParameterDef(object):
     @abstractmethod
     def compare_values(self, one, two):
         """
-        Compare values one and two of this datatype. It has to follow
-        the same return semantics as the Python standard __cmp__ methods,
-        meaning it returns negative integer if one < two, zero if one == two,
-        a positive integer if one > two.
+        Compare values one and two of this datatype.
 
-        :param one: the first value used in comparison
-        :param two: the second value used in comparison
-        :return:
-            Returns  negative integer if one < two,
-            zero if one == two, a positive integer if one > two
+        It has to follow the same return semantics as the Python standard
+        __cmp__ methods, meaning it returns negative integer if one < two,
+        zero if one == two, a positive integer if one > two.
+
+        Parameters
+        ----------
+        one: object in parameter definition
+            The first value used in comparison.
+
+        two: object in parameter definition
+            The second value used in comparison.
+
+        Returns
+        -------
+        comp: integer
+            comp < 0 iff one < two.
+            comp = 0 iff one = two.
+            comp > 0 iff one > two.
         """
         pass
 
 
 class NominalParamDef(ParamDef):
+    """
+    This defines a nominal parameter definition.
+
+    A nominal parameter definition is defined by the values as given in the
+    init function. These are a list of possible values it can take.
+    """
     values = None
 
     def __init__(self, values):
+        """
+        Instantiates the NominalParamDef instance.
+
+        Parameters
+        ----------
+        values: list
+            A list of values which are the possible values that are in this
+            parameter definition.
+
+        Raises
+        ------
+        ValueError:
+            Iff values is not a list, or values is an empty list.
+        """
         if not isinstance(values, list):
             raise ValueError(
                 "You created a NominalParameterDef object without "
@@ -53,58 +101,47 @@ class NominalParamDef(ParamDef):
         if len(values) < 1:
             raise ValueError(
                 "You need to specify a list of all possible values for this "
-                "data type in order to make it beeing used for your "
-                "optimization! The given list was empy: " + str(values)
+                "data type in order to make it being used for your "
+                "optimization! The given list was empty: " + str(values)
             )
 
         self.values = values
 
     def is_in_parameter_domain(self, value):
+        """
+        Tests whether value is in self.values as defined during the init
+        function.
+        """
         return value in self.values
 
 
 class OrdinalParamDef(NominalParamDef, ComparableParameterDef):
+    """
+    Defines an ordinal parameter definition.
+
+    This class inherits from NominalParamDef and ComparableParameterDef, and
+    consists of basically a list of possible values with a defined order. This
+    defined order is simply the order in which the elements are in the list.
+    """
     def __init__(self, values):
         super(OrdinalParamDef, self).__init__(values)
-
-        # to check comparability execute comparison for two random values
-        # from the list
-        try:
-            self.compare_values(random.choice(self.values),
-                                random.choice(self.values))
-        except:
-            raise ValueError("Creation of a OrdinalParamDef parameter not "
-                "possible for the values you specified. There was an error "
-                "during comparison. Your values need to be comparable. To "
-                "make sure they are the best way is to implement __cmp__ in "
-                "your data type. If you can't do this, you need to a non-"
-                "comparable param def such as NominalParamDef")
 
     def compare_values(self, one, two):
         """
         Compare values of this ordinal data type. Return is the same
         semantic as in __cmp__.
-        Comparison takes place based on the index
-        the given values one and two have in the values list in this object.
-        Meaning if this ordinal parameter definition has a values list of
-        '[3,5,1,4]', then '5' will be considered smaller than '1' and '1'
-        bigger than '5' because the index of '1' in this list is higher than
-        the index of '5'.
 
-
-        :param one: the first value used in comparison
-        :param two: the second value used in comparison
-        :return:
-            Returns  negative integer if one < two,
-            zero if one == two, a positive integer if one > two
+        Comparison takes place based on the index the given values one and
+        two have in the values list in this object. Meaning if this ordinal
+        parameter definition has a values list of [3,5,1,4]', then '5' will be
+        considered smaller than '1' and '1' bigger than '5' because the index
+        of '1' in this list is higher than the index of '5'.
         """
         if one not in self.values or two not in self.values:
             raise ValueError(
                 "Values not comparable! Either one or the other is not in the "
                 "values domain")
 
-        # if both values exist in list forward comparision to __cmp__ of
-        # integer type of list index
         if self.values.index(one) < self.values.index(two):
             return -1
         if self.values.index(one) > self.values.index(two):
@@ -114,23 +151,73 @@ class OrdinalParamDef(NominalParamDef, ComparableParameterDef):
 
 
 class NumericParamDef(ParamDef, ComparableParameterDef):
+    """
+    This class defines a numeric parameter definition.
+
+    It is characterized through the existence of a warp_in and a warp_out
+    function. The warp_in function squishes the whole parameter space to the
+    unit space [0, 1], while the warp_out function reverses this.
+    Note that it is necessary that
+        x = warp_in(warp_out(x)) for x in [0, 1] and
+        x = warp_out(warp_in(x)) for x in allowed parameter space.
+    """
     warping_in = None
     warping_out = None
 
     def __init__(self, warping_in, warping_out):
+        """
+        Initializes the Numeric Param Def.
+
+        Parameters
+        ----------
+        warping_in: function
+            warping_in must take a value and return a corresponding value in
+            the [0, 1] space.
+        warping_out: function
+            warping_out is the reverse function to warping_in.
+        """
+        super(NumericParamDef, self).__init__()
         self.warping_in = warping_in
         self.warping_out = warping_out
 
-    #TODO add exception catching for not in warping_out.
     def is_in_parameter_domain(self, value):
-        if self.warping_out(0) <= value <= self.warping_out(1):
+        """
+        Uses the warp_out function for tests.
+        """
+        if self.warp_out(0) <= value <= self.warp_out(1):
             return True
         return False
 
     def warp_in(self, value_in):
+        """
+        Warps value_in into the [0, 1] space.
+
+        Parameters
+        ----------
+        value_in: float
+            The input value
+
+        Returns
+        -------
+        value_in_scaled: float in [0, 1]
+            The scaled output value.
+        """
         return self.warping_in(value_in)
 
     def warp_out(self, value_out):
+        """
+        Warps value_out out of the [0, 1] space.
+
+        Parameters
+        ----------
+        value_out: float in [0, 1]
+            The output value.
+
+        Returns
+        -------
+        value_out_unscaled: float
+            The unscaled value in the parameter space.
+        """
         return self.warping_out(value_out)
 
     def compare_values(self, one, two):
@@ -149,11 +236,24 @@ class NumericParamDef(ParamDef, ComparableParameterDef):
 
 
 class LowerUpperNumericParamDef(NumericParamDef):
-
+    """
+    Defines a numeric parameter definition defined by a lower and upper bound.
+    """
     x_min = None
     x_max = None
 
     def __init__(self, lower_bound, upper_bound):
+        """
+        Initializes the lower/upper bound defined parameter space.
+
+        Parameters
+        ----------
+        lower_bound: float
+            The lowest possible value
+
+        upper_bound: float
+            The highest possible value
+        """
         self.x_min = lower_bound
         self.x_max = upper_bound
 
