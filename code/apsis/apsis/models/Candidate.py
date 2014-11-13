0c74c5938c0e3d19723cec4dfa0e210bcd860b43
Andreas Jauch <AJAUCH@de.ibm.com>
2014-11-13 15:24:53 +0100

    Fixes #39 - Docu and Cleanup

--- code/apsis/apsis/models/Candidate.py
+++ code/apsis/apsis/models/Candidate.py
@@ -5,12 +5,15 @@ import numpy as np
 
 class Candidate(object):
     """
-    Represents a candidate, that is a set of parameters which we are to check
-    plus metadata.
+    Represents a candidate, that is in a mathematical sense a single vector of
+    parameter values that is used in optimization.
 
+    Detailed Information
+    --------------------
     This class is used for optimizer-worker communications, and intra-optimizer
-     storage. It stores the parameters,
-    the result, cost and validity of the value and possesses a field for
+    storage. It stores the parameter vector, the result that has been achieved
+    on the problem when using this particular parameter vecotr,
+    cost and validity of the value and an optional field for identifying
     worker-relevant data.
     """
 
@@ -28,22 +31,29 @@ class Candidate(object):
 
         It requires only the used parameter vector, the rest are optional
         information.
-        :param params: A list of parameter values
-        representing this candidate's parameters used.
-        :param params_used: An (optional) vector of boolean values specifying
-        whether a certain parameter from params
-        is used. Use this function for example when describing a neural network
-        where information about the third hidden layer is irrelevant when
-        having less than three layers used.
-        :param result: The currently achieved result for this candidate. May
-        be None in the beginning.
-        :param valid: Whether the candidate's result is valid.
-        This may be False if, for example, the function to optimize crashes at
-        certain (unknown) values.
-        :param worker_information: A dictionary representing information for
-        the workers. Keys should be strings.
-        Can be used to, for example, store file paths where information
-        necessary for continuation is stored.
+
+        Parameters
+        ----------
+        params: vector
+            A list of parameter values representing this candidate's parameters
+            used.
+        params_used: vector of booleans
+            An (optional) vector of boolean values specifying whether a certain
+            parameter from params is used.
+            Use this function for example when describing a neural network
+            where information about the third hidden layer is irrelevant when
+            having less than three layers used.
+        result: float
+            The currently achieved result for this candidate. May be None in
+            the beginning.
+        valid: bool
+            Whether the candidate's result is valid. This may be False if, for
+            example, the function to optimize crashes at certain (unknown)
+            values.
+        worker_information: dict of string keys
+            A dictionary representing information for the workers.
+            Keys should be strings. Can be used to, for example, store file
+            paths where information necessary for continuation is stored.
         """
         #TODO add parameter validity check.
         self.params = params
@@ -66,8 +76,16 @@ class Candidate(object):
 
         Candidates are defined as being equal iff their params vectors are
         equal.
-        :param other: The other Candidate.
-        :return: True iff the Candidates are equal.
+
+        Parameters
+        ----------
+        other: Candidate
+            The other Candidate to be compared to.
+
+        Returns
+        -------
+            equal: bool
+                True iff the Candidates are equal. False otherwise.
         """
         if not isinstance(other, Candidate):
             return False
@@ -82,14 +100,21 @@ class Candidate(object):
         The list is first converted to string, then hashed. Since lists
         are not hashable in python.
 
-        :return: a hash value
+        Returns
+        ----------
+        hash_value: String
+            Hashed string of the string representing this Candidate vecotr of
+            hyper params.
+
+        :return:
         """
         #TODO this is a bit odd at the moemnt, since there is a good reason for
         #lists not beeing hashable. Hence to make this better we should have
         #a element by element __eq__ method, then this __hash__ makes more
         #  sense
+        hash_value = hash(str(self.params))
 
-        return hash(str(self.params))
+        return hash_value
 
 
     def __str__(self):
@@ -97,7 +122,9 @@ class Candidate(object):
         Stringifies the object. Currently only prints the parameters and the
         result value.
 
-        :return:
+        Returns
+        ----------
+        string: stringified object
         """
         string = "\n"
         string += "params: " + str(self.params) + "\n"
@@ -106,15 +133,26 @@ class Candidate(object):
 
     def __lt__(self, other):
         """
-        Compares two Candidate instances.
+        Compares two Candidate instances. Compares their result fields.
 
-        Compares their result fields.
         Parameters
         ----------
+        other: Candidate
+            A candidate that shall be used for comparison with this object.
 
-        other : Candidate
-            The Candidate with whom to compare.
+        Returns
+        ----------
+            lt: bool
+                True if the result of this candidate is smaller than
+                the other candidate's result.
+
+        Raises
+        ------
+        ValueError:
+            If other is not a Candidate object.
         """
+        #TODO Candidate lt is not consistent with candidate equals! Comparison
+        #of results with < is not such a good idea!
         if not isinstance(other, Candidate):
             raise ValueError("Is not compared with a Candidate, but with "
                              + str(other) + ".")
@@ -123,4 +161,14 @@ class Candidate(object):
         return False
 
     def as_vector(self):
+        """
+        Convert this Candidate's parameter vector to a numpy array.
+
+        Returns
+        ----------
+            params: numpy.array
+                The parameter vector represented by this Candidate object
+                as numpy array.
+
+        """
         return np.array(self.params)
