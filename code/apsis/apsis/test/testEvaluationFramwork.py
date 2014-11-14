a6ff02b8232a5a8e51e9c349358ca2d9117630f3
Frederik Diehl <frederikdiehl@gmail.com>
2014-11-14 14:21:27 +0100

    added multidim test for evaluation framework.

--- code/apsis/apsis/test/testEvaluationFramwork.py
+++ code/apsis/apsis/test/testEvaluationFramwork.py
@@ -5,16 +5,19 @@ from apsis.RandomSearchCore import RandomSearchCore
 from apsis.SimpleBayesianOptimizationCore import SimpleBayesianOptimizationCore
 import math
 import logging
+import time
 
 def obj_func(candidate):
     x = candidate.params[0]
-    return math.sin(x) * x**2
+    y = candidate.params[1]
+    return math.sin(x) * y**2
 
 logging.basicConfig(level=logging.DEBUG)
 func = obj_func
 
 grid = PreComputedGrid()
-param_defs = [LowerUpperNumericParamDef(0., 10.)]
+param_defs = [LowerUpperNumericParamDef(0., 10.),
+              LowerUpperNumericParamDef(0., 10.)]
 grid.build_grid_points(param_defs=param_defs, dimensionality=1000)
 grid.precompute_results(func)
 ev = EvaluationFramework()
@@ -22,7 +25,7 @@ ev = EvaluationFramework()
 optimizers = [RandomSearchCore({"param_defs": param_defs}),
               SimpleBayesianOptimizationCore({"param_defs": param_defs,
                                               "initial_random_runs": 5})]
-steps = 20
+steps = 10
 ev.plot_precomputed_grid(optimizers, ["random", "bayes"], grid, steps)
 
 raw_input("FINISHED")
