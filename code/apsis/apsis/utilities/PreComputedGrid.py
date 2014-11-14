4cc2f01e7190744495413e2e838407b22dc3a9f8
Frederik Diehl <frederikdiehl@gmail.com>
2014-11-14 11:47:04 +0100

    #35. Added evaluate_candidate to PreComputedGrid.

--- code/apsis/apsis/utilities/PreComputedGrid.py
+++ code/apsis/apsis/utilities/PreComputedGrid.py
@@ -5,11 +5,13 @@ import pickle
 
 class PreComputedGrid(object):
     grid_points = None
+    param_defs = None
 
     def __init__(self):
         self.grid_points = []
 
     def build_grid_points(self, param_defs, dimensionality):
+        self.param_defs = param_defs
         dimensionality_per_param = None
         if not isinstance(dimensionality, list):
             dimensionality_per_param = [dimensionality]*len(param_defs)
@@ -60,10 +62,33 @@ class PreComputedGrid(object):
             self.grid_points[i].result = objective_func(self.grid_points[i])
 
     def load_from_disk(self, filename):
-        self.grid_points = pickle.load(open(filename, "rb"))
+        self.grid_points, self.param_defs = pickle.load(open(filename, "rb"))
 
     def save_to_disk(self, filename):
-        pickle.dump(self.grid_points, open(filename, "wb"))
+        pickle.dump((self.grid_points, self.param_defs), open(filename, "wb"))
 
+    def get_closest_grid_candidate(self, candidate_in):
+        closest_grid_candidate = self.grid_points[0]
+        closest_distance = self.cand_distance(candidate_in, self.grid_points[0])
+        for i in range(len(self.grid_points)):
+            dist = self.cand_distance(candidate_in, self.grid_points[i])
+            if dist < closest_distance:
+                closest_grid_candidate = self.grid_points[i]
+                closest_distance = dist
+        #we found the closest grid candidate.
+        return closest_grid_candidate
 
+    def evaluate_candidate(self, candidate_in):
+        closest_grid_candidate = self.get_closest_grid_candidate(candidate_in)
+        candidate_in.result = closest_grid_candidate.result
+        for i in range(len(candidate_in.params)):
+            candidate_in.params[i] = closest_grid_candidate.params[i]
+        return candidate_in
 
+    def cand_distance(self, candidateA, candidateB):
+        distance = 0
+        for i in range(len(self.param_defs)):
+            distance += (self.param_defs[i].distance(candidateA.params[i],
+                                                    candidateB.params[i]))**2
+        distance **= 0.5
+        return distance
