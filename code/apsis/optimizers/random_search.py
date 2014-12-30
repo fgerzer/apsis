ca70f3cc465d55cdbfcea0819933d29e22dfb3cb
Frederik Diehl <frederikdiehl@gmail.com>
2014-12-30 14:55:14 +0100

    First work on random search.

---
+++ code/apsis/optimizers/random_search.py
@@ -0,0 +1,66 @@
+__author__ = 'Frederik Diehl'
+
+from apsis.optimizers.optimizer import Optimizer
+from apsis.models.parameter_definition import *
+from apsis.utilities.randomization import check_random_state
+from apsis.models.candidate import Candidate
+
+class RandomSearch(Optimizer):
+    """
+    Implements a random searcher for parameter optimization.
+
+    Attributes
+    ----------
+    random_state: None, int or random_state
+        The random_state used to generate random numbers.
+    """
+
+    SUPPORTED_PARAM_TYPES = [NominalParamDef, NumericParamDef]
+
+    random_state = None
+
+    def __init__(self, optimizer_arguments=None):
+        """
+        Initializes the RandomSearch.
+
+        Parameters
+        ----------
+        optimizer_arguments=None: dict or None
+            A dictionary of parameters for the optimizer. The following keys
+            are used:
+            "random_state"=None: random_state, None or int
+                A numpy random_state (after which it is modelled). Can be used
+                for repeatability.
+        """
+        if optimizer_arguments is None:
+            optimizer_arguments = {}
+        self.random_state = optimizer_arguments.get("random_state", None)
+
+    def get_next_candidates(self, experiment):
+        self.random_state = check_random_state(self.random_state)
+        value_dict = {}
+        for key, param_def in enumerate(experiment.parameter_definitions):
+            value_dict[key] = self._gen_param_val(param_def)
+        return Candidate(value_dict)
+
+    def _gen_param_val(self, param_def):
+        """
+        Returns a random parameter value for param_def.
+
+        Parameters
+        ----------
+        param_def: ParamDef
+            The parameter definition from which to choose one at random. The
+            following may happen:
+            NumericParamDef: warps_out a uniform 0-1 chosen value.
+            NominalParamDef: chooses one of the values at random.
+
+        Returns
+        -------
+        param_val:
+            The generated parameter value.
+        """
+        if isinstance(param_def, NumericParamDef):
+            return param_def.warp_out(self.random_state.uniform(0, 1))
+        elif isinstance((param_def, NominalParamDef)):
+            return self.random_state.choice(param_def.values)
