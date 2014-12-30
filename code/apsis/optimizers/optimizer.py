cf514d959389606e7edcc7318a507ef957d96375
Frederik Diehl <frederikdiehl@gmail.com>
2014-12-30 14:40:44 +0100

    Added function to check whether an experiment is supported by the optimizer.

--- code/apsis/optimizers/optimizer.py
+++ code/apsis/optimizers/optimizer.py
@@ -10,6 +10,8 @@ class Optimizer(object):
     """
     __metaclass__ = ABCMeta
 
+    SUPPORTED_PARAM_TYPES = []
+
     @abstractmethod
     def __init__(self, optimizer_params):
         """
@@ -41,3 +43,44 @@ class Optimizer(object):
             The Candidate to next evaluate.
         """
         pass
+
+    def _is_experiment_supported(self, experiment):
+        """
+        Tests whether all parameter types in experiment are supported by this
+        optimizer.
+
+        Parameters
+        ----------
+        experiment: Experiment
+            The experiment to test.
+
+        Returns:
+        supported: bool
+            False iff one or more of experiment's parameter definitions are not
+            supported.
+        """
+        for name, pd in enumerate(experiment.parameter_definitions):
+            if not self._is_supported_param_type(pd):
+                return False
+        return True
+
+    def _is_supported_param_type(self, param):
+        """
+        Tests whether a certain parameter is supported by the optimizer.
+
+        Parameters
+        ----------
+        param:
+            The parameter to be tested
+
+        Result
+        ------
+        is_supported: bool
+            True iff param is supported by this optimizer.
+        """
+        if isinstance(self.SUPPORTED_PARAM_TYPES, list):
+            for sup in self.SUPPORTED_PARAM_TYPES:
+                if isinstance(param, sup):
+                    return True
+
+        return False
