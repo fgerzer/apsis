fa23302d8f4d0b7a0412d469509c9f67b21ef798
Frederik Diehl <frederikdiehl@gmail.com>
2015-01-05 20:02:34 +0100

    Reworked BExpAssistant.

--- code/apsis/tests/test_experiment_assistant.py
+++ code/apsis/tests/test_experiment_assistant.py
@@ -1,20 +1,24 @@
 __author__ = 'Frederik Diehl'
 
-from apsis.assistants.experiment_assistant import BasicExperimentAssistant
+from apsis.assistants.experiment_assistant import PrettyExperimentAssistant
 from apsis.models.parameter_definition import *
+import math
+import logging
 
 def func(x, y):
-    return x+y
+    return x*math.cos(10*y)
 
 param_defs = {
     "x": LowerUpperNumericParamDef(0, 1),
-    "y": LowerUpperNumericParamDef(0, 1)
+    "y": LowerUpperNumericParamDef(0, 5)
 }
 
-BAss = BasicExperimentAssistant("RandomSearch", param_defs=param_defs)
+logging.basicConfig(level=logging.DEBUG)
+
+BAss = PrettyExperimentAssistant("test", "RandomSearch", param_defs=param_defs)
 results = []
 
-for i in range(20):
+for i in range(50):
     to_eval = BAss.get_next_candidate()
     print(to_eval)
     result = func(to_eval.params["x"], to_eval.params["y"])
@@ -24,3 +28,10 @@ for i in range(20):
 
 print(results)
 print(BAss.get_best_candidate())
+x, y, z = BAss._best_result_per_step_data()
+print("XXXX")
+print(x)
+print(y)
+print(z)
+print("ZZZZ")
+BAss.plot_result_per_step(plot_at_least=0.5)
