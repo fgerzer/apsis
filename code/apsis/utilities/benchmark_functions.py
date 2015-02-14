edbddc4a5fc1dd80f596fa5b67ba7ba0860d1899
Frederik Diehl <frederikdiehl@gmail.com>
2015-02-14 15:44:01 +0100

    Working on #92. #78. Added docu to benchmark_functions.

--- code/apsis/utilities/benchmark_functions.py
+++ code/apsis/utilities/benchmark_functions.py
@@ -1,27 +1,30 @@
 import math
 
-def branin_func(x, y):
+def branin_func(x, y, a=1, b=5.1/(4*math.pi**2), c=5/math.pi, r=6, s=10,
+                t=1/(8*math.pi)):
         """
-        Branin hoo function according to http://www.sfu.ca/~ssurjano/branin.html
+        Branin hoo function.
+
+        This is the same function as in
+        http://www.sfu.ca/~ssurjano/branin.html. The default parameters are
+        taken from that same site.
+
+        With the default parameters, there are three minima with f(x)=0.397887:
+        (-pi, 12.275), (pi, 2.275) and (9.42478, 2.475).
 
         Paramters
         ---------
-        x: float
+        x : float
             A real valued float
-        y: float
+        y : float
             A real valued float
-
+        a, b, c, r, s, t : floats, optional
+            Parameters for the shape of the Branin hoo function. Thier default
+            values are according to the recommendations of the above website.
         Returns
         -------
-        result: float
+        result : float
             A real valued float.
         """
-        a = 1
-        b = 5.1/(4*math.pi**2)
-        c = 5/math.pi
-        r = 6
-        s = 10
-        t = 1/(8*math.pi)
         result = a*(y-b*x**2+c*x-r)**2 + s*(1-t)*math.cos(x)+s
-        #print("Branin: %f" %result)
         return result
