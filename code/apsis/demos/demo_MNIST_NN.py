18d5f26ddfc4fc3438e4de64317058ce513bc1f5
Frederik Diehl <frederikdiehl@gmail.com>
2015-03-14 19:49:09 +0100

    Added the MNIST neural network demo including docs.

--- code/apsis/demos/demo_MNIST_NN.py
+++ code/apsis/demos/demo_MNIST_NN.py
@@ -3,6 +3,8 @@ __author__ = 'Frederik Diehl'
 # Code for the NN adapted from the breze library example.
 # For Breze, see github.com/breze-no-salt/breze.
 
+# Note that you need Breze, Climin and Theano for this to work.
+
 import cPickle
 import gzip
 import time
@@ -27,6 +29,20 @@ logger = get_logger("apsis.demos.demo_MNIST_NN")
 start_time = None
 
 def load_MNIST():
+    """
+    Loads MNIST from the datafile under ./mnist.pkl.gz.
+
+    Returns
+    -------
+    X, Z : matrix
+        Feature and Target matrices of the training set, one-hot encoded.
+    VX, VZ : matrix
+        Feature and Target matrices of the validation set, one-hot encoded.
+    TX, TZ : matrix
+        Feature and Target matrices of the test set, one-hot encoded.
+    image_dims : tuple
+        Dimensions of the image
+    """
     datafile = 'mnist.pkl.gz'
     # Load data.
 
@@ -44,13 +60,38 @@ def load_MNIST():
     return X, Z, VX, VZ, TX, TZ, image_dims
 
 def do_one_eval(X, Z, VX, VZ, step_rate, momentum, decay, c_wd):
+    """
+    Does one evaluation of a neural network with the above parameters.
+
+    Parameters
+    ----------
+    X, Z : matrix
+        Feature and Target matrices of the training set, one-hot encoded.
+    VX, VZ : matrix
+        Feature and Target matrices of the validation set, one-hot encoded.
+    step_rate : float
+        The step-rate/learning rate of the rmsprop-algorithm
+    momentum : float
+        The momentum of the rmsprop.
+    decay : float
+        The step-rate decay
+    c_wd : float
+        Penalty term for the weight
+
+    Returns
+    -------
+    val_emp : float
+        The percentage of wrongly classified samples.
+    """
+
     max_passes = 100
     batch_size = 250
     max_iter = max_passes * X.shape[0] / batch_size
     n_report = X.shape[0] / batch_size
     optimizer = 'rmsprop', {'step_rate': step_rate, 'momentum': momentum, 'decay': decay}
-    #optimizer = 'adam'
-    #optimizer = 'gd', {'steprate': 0.1, 'momentum': climin.schedule.SutskeverBlend(0.99, 250), 'momentum_type': 'nesterov'}
+
+    # This defines our NN. Since BayOpt does not support categorical data, we just
+    # use a fixed hidden layer length and transfer functions.
     m = Mlp(784, [800], 10, hidden_transfers=['sigmoid'], out_transfer='softmax', loss='cat_ce',
             optimizer=optimizer, batch_size=batch_size)
     climin.initialize.randomize_normal(m.parameters.data, 0, 1e-1)
@@ -86,10 +127,26 @@ def do_one_eval(X, Z, VX, VZ, step_rate, momentum, decay, c_wd):
             'val_emp': f_n_wrong(VX, VZ),
         })
         row = '%(n_iter)i\t%(time)g\t%(loss)g\t%(val_loss)g\t%(train_emp)g\t%(val_emp)g' % info
+
+        # Comment in this row if you want updates during the computation.
         #print row
     return info["val_emp"]
 
 def do_evaluation(LAss, opt, X, Z, VX, VZ):
+    """
+    Evaluates opt on a certain parameter set.
+
+    Parameters
+    ----------
+    LAss : ValidationLabAssistant
+        The LAss to use for updates.
+    opt : string
+        The name of the experiment to use here.
+    X, Z : matrix
+        Feature and Target matrices of the training set, one-hot encoded.
+    VX, VZ : matrix
+        Feature and Target matrices of the validation set, one-hot encoded.
+    """
     to_eval = LAss.get_next_candidate(opt)
     step_rate = to_eval.params["step_rate"]
     momentum = to_eval.params["momentum"]
@@ -101,8 +158,23 @@ def do_evaluation(LAss, opt, X, Z, VX, VZ):
 
 
 def demo_on_MNIST(random_steps, steps, cv=1):
+    """
+    Demos the learning on MNIST.
+
+    Parameters
+    ----------
+    random_steps : int
+        The number of initial random steps. This is shared by all optimizers.
+    steps : int
+        The number of total steps. Must be greater than random_steps.
+
+    cv : int
+        The number of crossvalidations for evaluation.
+    """
     X, Z, VX, VZ, TX, TZ, image_dims = load_MNIST()
 
+    # The current parameter definitions are the more successful ones.
+    # However, this can be compared by changing the lines commented.
     param_defs = {
         #"step_rate": MinMaxNumericParamDef(0, 1),
         "step_rate": AsymptoticNumericParamDef(0, 1),
@@ -115,9 +187,7 @@ def demo_on_MNIST(random_steps, steps, cv=1):
     LAss = ValidationLabAssistant(cv=cv)
     experiments = ["random_mnist", "bay_mnist_ei_L-BFGS-B"]#, "bay_mnist_ei_rand"]
     LAss.init_experiment("random_mnist", "RandomSearch", param_defs, minimization=True)
-    #LAss.init_experiment("bay_mnist_ei_rand", "BayOpt", param_defs,
-    #                     minimization=True, optimizer_arguments=
-    #    {"acquisition_hyperparams":{"optimization": "random"}})
+
     global start_time
     start_time = time.time()
     #First, the random steps
@@ -125,8 +195,6 @@ def demo_on_MNIST(random_steps, steps, cv=1):
         print("%s\tBeginning with random initialization. Step %i/%i" %(str(time.time()-start_time), i, random_steps*cv))
         do_evaluation(LAss, "random_mnist", X, Z, VX, VZ)
 
-    #clone
-    #LAss.init_experiment("bay_mnist_ei_L-BFGS-B", "BayOpt", param_defs, minimization=True)
     LAss.clone_experiments_by_name(exp_name=experiments[0], new_exp_name=experiments[1],
                                optimizer="BayOpt",
                                optimizer_arguments={"initial_random_runs": random_steps})
