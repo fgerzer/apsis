a226bc38df9b88c1a8874a7a24000a91b50e243a
Frederik Diehl <frederikdiehl@gmail.com>
2015-08-20 10:43:53 +0200

    Added conversion to and from dicts to param_def_utilities.

--- code/apsis/utilities/param_def_utilities.py
+++ code/apsis/utilities/param_def_utilities.py
@@ -1,25 +1,25 @@
 __author__ = 'Frederik Diehl'
 
-from apsis.models.parameter_definition import *
+import apsis.models.parameter_definition as pd
 
-AVAILABLE_PARAM_DEFS = {
-    "nominal_param_def": NominalParamDef,
-    "ordinal_param_def": OrdinalParamDef,
-    "numeric_param_def": NumericParamDef,
-    "min_max_numeric_param_def": MinMaxNumericParamDef,
-    "position_param_def": PositionParamDef,
-    "fixed_value_param_def": FixedValueParamDef,
-    "anymptotic_numeric_param_def": AsymptoticNumericParamDef
-}
+def param_defs_to_dict(param_defs):
+    param_dict = {}
+    for k in param_defs:
+        param_dict[k] = _param_def_to_dict(param_defs[k])
+    return param_dict
 
-def convert_param_defs(param_def_dict_raw):
-    param_def_dict = {}
-    for p in param_def_dict:
-        param_def_dict[p] = convert_param_def(param_def_dict_raw[p])
-    return param_def_dict
+def _param_def_to_dict(param_def):
+    dict = param_def.to_dict()
+    dict["type"] = param_def.__class__.__name__
+    return dict
 
-def convert_param_def(param_def_dict):
-    type = param_def_dict["type"]
-    del param_def_dict["type"]
-    class_type = AVAILABLE_PARAM_DEFS[type]
-    return class_type(**param_def_dict)
+def dict_to_param_defs(dict):
+    param_defs = {}
+    for k in dict:
+        param_defs[k] = _param_def_to_dict(dict[k])
+    return param_defs
+
+def _dict_to_param_def(dict):
+    param_type = getattr(pd, dict["type"])
+    del dict["type"]
+    return param_type(**dict)
