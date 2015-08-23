19c8785fb33a7e7d4185cf3fd4f48550f11e9ab2
Frederik Diehl <frederikdiehl@gmail.com>
2015-08-23 18:09:30 +0200

    #119. Added a blocking/timeout interface to apsis_client.

--- code/apsis_client/apsis_connection.py
+++ code/apsis_client/apsis_connection.py
@@ -1,12 +1,31 @@
 __author__ = 'Frederik Diehl'
 
 import requests
+import time
 
 class Connection(object):
     server_address = None
 
     def __init__(self, server_address):
         self.server_address = server_address
+
+    def request(self, request, url, json=None, blocking=False, timeout=10):
+        start_time = time.time()
+        while timeout <= 0 or time.time()-start_time < timeout:
+            if json is None:
+                r = request(url=url)
+            else:
+                r = request(url=url, json=None)
+            if blocking:
+                if r.json()["result"] is None:
+                    time.sleep(0.1)
+                    continue
+            #if not blocking:
+            #    if r.json()["result"] is None:
+            #        return None
+            return r.json()["result"]
+
+
 
     def init_experiment(self, name, optimizer, param_defs, optimizer_arguments,
                         minimization=True):
@@ -29,10 +48,11 @@ class Connection(object):
 
     def get_next_candidate(self, exp_name):
         url = self.server_address + "/experiments/%s/get_next_candidate" %exp_name
-        r = requests.get(url=url)
-        if r.json()["result"] == "None":
-            return None
-        return r.json()["result"]
+        return self.request(requests.get, url=url, blocking=True)
+        #r = requests.get(url=url)
+        #if r.json()["result"] == "None":
+        #    return None
+        #return r.json()["result"]
 
     def update(self, exp_name, candidate, status):
         #TODO candidate currently as a dict.
