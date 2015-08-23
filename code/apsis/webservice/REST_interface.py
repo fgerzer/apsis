827ae87aac721dad42096d377b54bc9329d59e81
Frederik Diehl <frederikdiehl@gmail.com>
2015-08-23 20:15:35 +0200

    #106. Cleanup.

--- code/apsis/webservice/REST_interface.py
+++ code/apsis/webservice/REST_interface.py
@@ -1,18 +1,9 @@
 from flask import Flask, request, jsonify
-from flask_negotiate import consumes, produces
 from apsis.assistants.lab_assistant import LabAssistant
-#from apsis.models.parameter_definition import *
 from apsis.models.candidate import Candidate, from_dict
-#from apsis.models.experiment import Experiment
 from functools import wraps
-import multiprocessing
-from multiprocessing import reduction
-import traceback
 from apsis.utilities.param_def_utilities import dict_to_param_defs
 from apsis.utilities.logging_utils import get_logger
-#import json
-
-import logging
 
 WS_PORT = 5000
 CONTEXT_ROOT = ""
@@ -23,12 +14,14 @@ _logger = None
 
 lAss = None
 
+
 def start_apsis():
     global lAss, _logger
     _logger = get_logger("REST_interface")
     lAss = LabAssistant()
     app.run(debug=True)
-    print("Started app. Initialized LAss.")
+    _logger.info("Finished initialization. Interface running now.")
+
 
 def exception_handler(func):
     @wraps(func)
@@ -41,21 +34,20 @@ def exception_handler(func):
             return jsonify(result="failed")
     return handle_exception
 
+
 @app.route(CONTEXT_ROOT + "/", methods=["GET"])
-#@produces('application/json')
 def overview_page():
     """
     This will, later, become an overview over the experiment.
     """
     experiments = lAss.exp_assistants.keys()
     str(experiments)
+    raise NotImplementedError
     #TODO
 
 
 @app.route(CONTEXT_ROOT + "/experiments", methods=["POST"])
 @exception_handler
-#@consumes('application/json')
-#@produces('application/json')
 def init_experiment():
     """
     This initializes a single experiment.
@@ -82,7 +74,6 @@ def init_experiment():
         is minimization.
     }
     """
-
     data_received = request.get_json()
     data_received = _filter_data(data_received)
     name = data_received.get("name", None)
@@ -97,15 +88,18 @@ def init_experiment():
                          minimization)
     return "success"
 
+
 @app.route(CONTEXT_ROOT + "/experiments", methods=["GET"])
 @exception_handler
 def get_all_experiments():
     return lAss.exp_assistants.keys()
 
+
 @app.route(CONTEXT_ROOT + "/experiments/<experiment_id>", methods=["GET"])
 def get_experiment(experiment_id):
-   pass
+   raise NotImplementedError
     #TODO
+
 
 @app.route(CONTEXT_ROOT + "/experiments/<experiment_id>"
                           "/get_next_candidate", methods=["GET"])
@@ -113,10 +107,11 @@ def get_experiment(experiment_id):
 def get_next_candidate(experiment_id):
     result_cand = lAss.get_next_candidate(experiment_id)
     if result_cand is None:
-        result = "failed"#"None"
+        result = "failed"
     else:
         result = result_cand.to_dict()
     return result
+
 
 @app.route(CONTEXT_ROOT + "/experiments/<experiment_id>"
                           "/get_best_candidate", methods=["GET"])
@@ -129,6 +124,7 @@ def get_best_candidate(experiment_id):
         result = result_cand.to_dict()
     return result
 
+
 @app.route(CONTEXT_ROOT + "/experiments/<experiment_id>"
                           "/update", methods=["POST"])
 @exception_handler
@@ -136,9 +132,9 @@ def update(experiment_id):
     data_received = request.get_json()
     status = data_received["status"]
     candidate = from_dict(data_received["candidate"])
-    #lAss.update(status=status, candidate=candidate)
     lAss.update(experiment_id, status=status, candidate=candidate)
     return "success"
+
 
 @app.route(CONTEXT_ROOT + "/experiments/<experiment_id>/candidates",
            methods=["GET"])
@@ -153,85 +149,8 @@ def _get_all_candidates(experiment_id):
     return result
 
 
-def _send_msg_lab(msg):
-    #msg["result_queue"] = reduction.reduce_connection(msg["result_queue"])
-    lqueue.put(msg)
-
 def _filter_data(json):
     for k in json:
         if isinstance(json[k], unicode):
             json[k] = str(json[k])
     return json
-#
-# @app.route(CONTEXT_ROOT + "/experiments/<experiment_id>/working", methods=["POST"])
-# @consumes('application/json')
-# @produces('application/json')
-# def working(experiment_id):
-#     """
-#     POST method to post new information from a worker targetting at the cores
-#     working method. Needs to be given the experiment_id of the experiment
-#     to which the result belongs.
-#
-#     POST DATA
-#     ---------
-#     candidate: dict
-#         dict representing Candidate object.
-#     status: String
-#         indicating status of this candidate: "finished", "working",...
-#     [worker_id]: String
-#         A string id describing a worker.
-#     [can_be_killed=False]: boolean
-#         If this worker can be killed.
-#     """
-#     logging.debug("POST experiments/<id>/working for experiment id " + experiment_id)
-#
-#     request_body = request.get_json()
-#
-#     if(request_body is None):
-#         return "ERORR (1) - request body empty", 500
-#
-#     #mandatory
-#     serialized_candidate = request_body.get('candidate', None)
-#     candidate_status = request_body.get('status', None)
-#
-#     #optional
-#     worker_id = request_body.get('worker_id', None)
-#     can_be_killed = request_body.get('can_be_killed', False)
-#
-#     if(serialized_candidate is not None and candidate_status is not None):
-#         deserialized_candidate = Candidate.from_dict(serialized_candidate)
-#         logging.debug("Deserialized candidate in state " +
-#                       str(type(candidate_status)) +  ": " +
-#                       str(candidate_status) + " "
-#                       + str(deserialized_candidate))
-#
-#         #TODO use experiment id to register working in the appropriate experiment
-#         #TODO call to OptimizationCoreInterface.working
-#         #TODO return the continue value for the experiment posted
-#         return jsonify({"continue": True}), 200
-#
-#     else:
-#         return "ERORR (2) - candidate object was not given", 500
-#
-#
-# @app.route(CONTEXT_ROOT + "/experiments/<experiment_id>/candidate", methods=["GET"])
-# @produces('application/json')
-# def next_candidate(experiment_id):
-#     logging.debug("GET experiments/<id>/candidate for experiment id " +
-#                   experiment_id)
-#
-#     #TODO use experiment id to obtain next candidate from
-#     #sample code for testing follows
-#     test_point = [1, 1, 1]
-#     for i in range(0, len(test_point)):
-#         test_point[i] = random.gauss(0, 10)
-#     test_candidate = Candidate(test_point)
-#
-#     #return test candidate as dict and then bring to json
-#     return jsonify(test_candidate.__dict__), 200
-#
-#
-# logging.info("APSIS web service starting at " + str(WS_PORT))
-# app.run(debug=True, port=WS_PORT)
-#
-#
