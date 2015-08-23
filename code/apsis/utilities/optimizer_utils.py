b399686bff5d42ed64824343c74e5fabaa237dab
Frederik Diehl <frederikdiehl@gmail.com>
2015-08-23 14:07:09 +0200

    Eliminated old check_optimizer, renamed build_queue_optimizer to check_optimizer. We don't have any distinction between them anymore.

--- code/apsis/utilities/optimizer_utils.py
+++ code/apsis/utilities/optimizer_utils.py
@@ -6,39 +6,13 @@ from apsis.optimizers.bayesian_optimization import SimpleBayesianOptimizer
 
 AVAILABLE_OPTIMIZERS = {"RandomSearch": RandomSearch}#, "BayOpt": SimpleBayesianOptimizer}
 
-def check_optimizer(optimizer, optimizer_arguments=None):
-    """
-    Returns the optimizer corresponding to optimizer.
-
-    If optimizer is an Optimizer, no action is taken.
-    If optimizer is a string corresponding to one of the optimizer names in
-    AVAILABLE_OPTIMIZERS, the corresponding optimizer is initialized.
-    Otherwise, raises a ValueError.
-
-    Parameters
-    ----------
-    optimizer : Optimizer or String
-        The optimizer - or string representing it - to return.
-    optimizer_arguments : dict or None, optional
-        The parameters for the optimizer. Will not be used if optimizer is
-        already an Optimizer.
-
-    Returns
-    -------
-    An Optimizer instance corresponding to optimizer.
-    """
+def check_optimizer(optimizer, experiment, out_queue, in_queue, optimizer_arguments=None):
+    #TODO documentation
     if isinstance(optimizer, Optimizer):
         return optimizer
-
+    if isinstance(optimizer, unicode):
+        optimizer = str(optimizer)
     if isinstance(optimizer, str) and optimizer in AVAILABLE_OPTIMIZERS.keys():
-        return AVAILABLE_OPTIMIZERS[optimizer](optimizer_arguments)
-
-    raise ValueError("No corresponding optimizer found for %s"
-                     %str(optimizer))
-
-def build_queue_optimizer(optimizer_name, experiment, out_queue, in_queue, optimizer_arguments=None):
-    #TODO documentation
-    if isinstance(optimizer_name, str) and optimizer_name in AVAILABLE_OPTIMIZERS.keys():
-        return AVAILABLE_OPTIMIZERS[optimizer_name](optimizer_arguments, experiment, out_queue, in_queue)
-    raise ValueError("No corresponding optimizer found for %s"
-                     %str(optimizer_name))
+        return AVAILABLE_OPTIMIZERS[optimizer](optimizer_arguments, experiment, out_queue, in_queue)
+    raise ValueError("No corresponding optimizer found for %s. Optimizer must "
+                     "be in %s" %(str(optimizer), AVAILABLE_OPTIMIZERS.keys()))
