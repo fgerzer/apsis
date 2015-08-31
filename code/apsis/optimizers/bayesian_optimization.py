873ec168c390752b3fb19b6d47978a5c4a7db95e
Frederik Diehl <frederikdiehl@gmail.com>
2015-08-31 09:54:19 +0200

    BayesianOptimizer: Added documentation.

--- code/apsis/optimizers/bayesian_optimization.py
+++ code/apsis/optimizers/bayesian_optimization.py
@@ -10,6 +10,38 @@ import GPy
 
 
 class BayesianOptimizer(Optimizer):
+    """
+    This is a bayesian optimizer class.
+
+    It is a subclass of Optimizer, and internally uses GPy.
+    Currently, it supports Numeric and PositionParamDefs, with support for
+    NominalParamDef needing to be integrated.
+
+    Attributes
+    ----------
+    SUPPORTED_PARAM_TYPES : list of ParamDefs
+        The supported parameter types. Currently only numeric and position.
+    kernel : GPy Kernel
+        The Kernel to be used with the gp.
+    acquisition_function : acquisition_function
+        The acquisition function to use
+    acquisition_hyperparams :
+        The acquisition hyperparameters.
+    random_state : scipy random_state or int.
+        The scipy random state or object to initialize one. For reproduction.
+    random_searcher : RandomSearch
+        The random search instance used to generate the first
+        initial_random_runs candidates.
+    gp : GPy gaussian process
+        The gaussian process used here.
+    initial_random_runs : int
+        The number of initial random runs before using the GP. Default is 10.
+    num_gp_restarts : int
+        GPy's optimization requires restarts to find a good solution. This
+        parameter controls this. Default is 10.
+    logger: logger
+        The logger instance for this object.
+    """
     SUPPORTED_PARAM_TYPES = [NumericParamDef, PositionParamDef]
 
     kernel = None
@@ -30,7 +62,33 @@ class BayesianOptimizer(Optimizer):
 
     _logger = None
 
-    def __init__(self, optimizer_params, experiment):
+    def __init__(self, experiment, optimizer_params=None):
+        """
+        Initializes a bayesian optimizer.
+        Parameters
+        ----------
+        experiment : Experiment
+            The experiment for which to optimize.
+        optimizer_arguments: dict of string keys
+            Sets the possible arguments for this optimizer. Available are:
+            "initial_random_runs" : int, optional
+                The number of initial random runs before using the GP. Default
+                is 10.
+            "random_state" : scipy random state, optional
+                The scipy random state or object to initialize one. Default is
+                None.
+            "acquisition_hyperparameters" : dict, optional
+                dictionary of acquisition-function hyperparameters
+            "num_gp_restarts" : int
+                GPy's optimization requires restarts to find a good solution.
+                This parameter controls this. Default is 10.
+            "acquisition" : AcquisitionFunction
+                The acquisition function to use. Default is
+                ExpectedImprovement.
+            "num_precomputed" : int
+                The number of points that should be kept precomputed for faster
+                multiple workers.
+        """
         self._logger = get_logger(self)
         if optimizer_params is None:
             optimizer_params = {}
@@ -51,12 +109,12 @@ class BayesianOptimizer(Optimizer):
         self.kernel_params = optimizer_params.get("kernel_params", {})
         self.kernel = optimizer_params.get("kernel", "matern52")
         self.random_searcher = RandomSearch(optimizer_params, experiment)
-        Optimizer.__init__(self, optimizer_params, experiment)
+        Optimizer.__init__(self, experiment, optimizer_params)
 
 
     def get_next_candidates(self, num_candidates=1):
         if len(self._experiment.candidates_finished) < self.initial_random_runs:
-            #we do a random search.
+            # we do a random search.
             return self.random_searcher.get_next_candidates(num_candidates)
         candidates = []
         new_candidate_points = self.acquisition_function.compute_proposals(
@@ -65,7 +123,7 @@ class BayesianOptimizer(Optimizer):
         )
         self.return_max = False
         for point_and_value in new_candidate_points:
-            #get the the candidate point which is the first entry in the tuple.
+            # get the the candidate point which is the first entry in the tuple.
             point_candidate = Candidate(self._experiment.warp_pt_out(point_and_value[0]))
             candidates.append(point_candidate)
         return candidates
@@ -79,7 +137,6 @@ class BayesianOptimizer(Optimizer):
         candidate_matrix = np.zeros((len(self._experiment.candidates_finished),
                                      len(self._experiment.parameter_definitions)))
         results_vector = np.zeros((len(self._experiment.candidates_finished), 1))
-
 
         param_names = sorted(self._experiment.parameter_definitions.keys())
         self.kernel = self._check_kernel(self.kernel, len(param_names),
