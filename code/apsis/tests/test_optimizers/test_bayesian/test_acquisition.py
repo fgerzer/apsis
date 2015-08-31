4040c91842edcc255e46d44039b7c5842970b0a5
Frederik Diehl <frederikdiehl@gmail.com>
2015-08-31 14:14:24 +0200

    Fixed the acquisition tests.

--- code/apsis/tests/test_optimizers/test_bayesian/test_acquisition.py
+++ code/apsis/tests/test_optimizers/test_bayesian/test_acquisition.py
@@ -1,6 +1,6 @@
 __author__ = 'Frederik Diehl'
 
-from apsis.optimizers.bayesian_optimization import SimpleBayesianOptimizer
+from apsis.optimizers.bayesian_optimization import BayesianOptimizer
 from nose.tools import assert_is_none, assert_equal, assert_dict_equal, \
     assert_true, assert_false
 from apsis.optimizers.bayesian.acquisition_functions import ExpectedImprovement, ProbabilityOfImprovement
@@ -8,27 +8,31 @@ from apsis.models.experiment import Experiment
 from apsis.models.parameter_definition import MinMaxNumericParamDef
 from apsis.models.candidate import Candidate
 
-class testAcqusitionFunction(object):
+class testAcquisitionFunction(object):
 
     def test_EI(self):
-        opt = SimpleBayesianOptimizer({"initial_random_runs": 3})
         exp = Experiment("test", {"x": MinMaxNumericParamDef(0, 1)})
-        for i in range(10):
-            cand = opt.get_next_candidates(exp)[0]
+        opt = BayesianOptimizer(exp, {"initial_random_runs": 3})
+
+        for i in range(5):
+            cand = opt.get_next_candidates()[0]
             assert_true(isinstance(cand, Candidate))
             cand.result = 2
             exp.add_finished(cand)
-        cands = opt.get_next_candidates(exp, num_candidates=3)
+            opt.update(exp)
+        cands = opt.get_next_candidates(num_candidates=3)
         assert_equal(len(cands), 3)
 
     def test_PoI(self):
-        opt = SimpleBayesianOptimizer({"initial_random_runs": 3, "acquisition": ProbabilityOfImprovement})
-        assert_true(isinstance(opt.acquisition_function, ProbabilityOfImprovement))
         exp = Experiment("test", {"x": MinMaxNumericParamDef(0, 1)})
-        for i in range(10):
-            cand = opt.get_next_candidates(exp)[0]
+        opt = BayesianOptimizer(exp, {"initial_random_runs": 3, "acquisition": ProbabilityOfImprovement})
+        assert_true(isinstance(opt.acquisition_function, ProbabilityOfImprovement))
+
+        for i in range(5):
+            cand = opt.get_next_candidates()[0]
             assert_true(isinstance(cand, Candidate))
             cand.result = 2
             exp.add_finished(cand)
-        cands = opt.get_next_candidates(exp, num_candidates=3)
+            opt.update(exp)
+        cands = opt.get_next_candidates(num_candidates=3)
         assert_equal(len(cands), 3)
