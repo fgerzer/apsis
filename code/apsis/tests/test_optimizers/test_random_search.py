f2c9cbadcbb247748743ddf38a44c29dfcbd366d
Frederik Diehl <frederikdiehl@gmail.com>
2015-08-31 14:24:55 +0200

    Fixed all optimizer tests.

--- code/apsis/tests/test_optimizers/test_random_search.py
+++ code/apsis/tests/test_optimizers/test_random_search.py
@@ -1,6 +1,6 @@
 __author__ = 'Frederik Diehl'
 
-from apsis.optimizers.random_search import RandomSearch, QueueRandomSearch
+from apsis.optimizers.random_search import RandomSearch
 from nose.tools import assert_is_none, assert_equal, assert_dict_equal, \
     assert_true, assert_false
 from apsis.models.experiment import Experiment
@@ -15,33 +15,17 @@ class test_RandomSearch(object):
 
     def test_init(self):
         #test initialization
-        opt = RandomSearch(None)
+        exp = Experiment("test", {"x": MinMaxNumericParamDef(0, 1)}, NominalParamDef(["A", "B", "C"]))
+        opt = RandomSearch(exp)
 
     def test_get_next_candidate(self):
-        opt = RandomSearch({"initial_random_runs": 3})
         exp = Experiment("test", {"x": MinMaxNumericParamDef(0, 1)}, NominalParamDef(["A", "B", "C"]))
+        opt = RandomSearch(exp, {"initial_random_runs": 3})
+
         for i in range(5):
-            cand = opt.get_next_candidates(exp)[0]
+            cand = opt.get_next_candidates()[0]
             assert_true(isinstance(cand, Candidate))
             cand.result = 2
             exp.add_finished(cand)
-        cands = opt.get_next_candidates(exp, num_candidates=3)
+        cands = opt.get_next_candidates(num_candidates=3)
         assert_equal(len(cands), 3)
-
-
-class test_ParallelRandomSearch(object):
-
-    def test_get_next_candidate(self):
-        out_queue = multiprocessing.Queue()
-        exp = Experiment("test", {"x": MinMaxNumericParamDef(0, 1)}, NominalParamDef(["A", "B", "C"]))
-        opt = QueueRandomSearch({}, exp, out_queue)
-        opt.start()
-        for i in range(5):
-            cand = out_queue.get()
-            assert_true(isinstance(cand, Candidate))
-            cand.result = 2
-            exp.add_finished(cand)
-        os.kill(opt.pid, signal.SIGINT)
-        time.sleep(0.5)
-        assert_false(opt.is_alive(), "Optimizer is still alive despite "
-                                     "being killed.")
