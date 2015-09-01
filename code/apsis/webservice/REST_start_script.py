eabf2792f95f4fe0300c603b0d5e80f97bf314db
Frederik Diehl <frederikdiehl@gmail.com>
2015-09-01 14:53:55 +0200

    Reworked start_script slightly.

--- code/apsis/webservice/REST_start_script.py
+++ code/apsis/webservice/REST_start_script.py
@@ -3,6 +3,11 @@ __author__ = 'Frederik Diehl'
 import REST_interface
 import sys
 
+def start_rest(validation=False, cv=5):
+    print("Initializing apsis. Val is %s, cv is %s" %(validation, cv))
+    REST_interface.start_apsis(validation)
+    print("Initialized apsis.")
+
 if __name__ == "__main__":
     validation = False
     cv = 5
@@ -11,6 +16,4 @@ if __name__ == "__main__":
             validation = True
         if sys.argv > 2:
             cv = int(sys.argv[2])
-    print("Initializing apsis. Val is %s, cv is %s" %(validation, cv))
-    REST_interface.start_apsis(validation)
-    print("Initialized apsis.")
+    start_rest(validation, cv)
