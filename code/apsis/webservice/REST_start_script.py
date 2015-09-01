96338b135d08d53106fb679dac18f36f96fae109
Frederik Diehl <frederikdiehl@gmail.com>
2015-09-01 12:02:18 +0200

    Added console params to REST_Start_script.

--- code/apsis/webservice/REST_start_script.py
+++ code/apsis/webservice/REST_start_script.py
@@ -1,10 +1,16 @@
 __author__ = 'Frederik Diehl'
 
-from REST_interface import app
 import REST_interface
-from apsis.assistants.lab_assistant import LabAssistant
-from multiprocessing.queues import Queue
-import multiprocessing
+import sys
 
-REST_interface.start_apsis()
-print("Initialized apsis.")
+if __name__ == "__main__":
+    validation = False
+    cv = 5
+    if sys.argv > 1:
+        if sys.argv[1] == "val":
+            validation = True
+        if sys.argv > 2:
+            cv = int(sys.argv[2])
+    print("Initializing apsis. Val is %s, cv is %s" %(validation, cv))
+    REST_interface.start_apsis(validation)
+    print("Initialized apsis.")
