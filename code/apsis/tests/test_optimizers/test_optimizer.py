5ba96d50198f269b4856833ab7821ef04a110d43
Frederik Diehl <frederikdiehl@gmail.com>
2015-10-02 01:28:12 +0200

    Added tests for the QueueBackend.

--- code/apsis/tests/test_optimizers/test_optimizer.py
+++ code/apsis/tests/test_optimizers/test_optimizer.py
@@ -1,10 +1,13 @@
 __author__ = 'Frederik Diehl'
 
-from apsis.optimizers.optimizer import Optimizer, QueueBasedOptimizer
+from apsis.optimizers.optimizer import Optimizer, QueueBasedOptimizer, \
+    QueueBackend
 from apsis.models.experiment import Experiment
 from apsis.models.parameter_definition import *
 from nose.tools import assert_raises
 from apsis.optimizers.random_search import RandomSearch
+from multiprocessing import Queue
+import time
 
 class TestOptimizer(object):
     optimizer = None
@@ -68,3 +71,38 @@ class TestQueueOptimizer(object):
 
     def teardown(self):
         self.optimizer.exit()
+
+
+class TestQueueBackend(object):
+    backend = None
+    experiment = None
+
+    def setup(self):
+        param_def = {
+            "x": MinMaxNumericParamDef(0, 1)
+        }
+        self.experiment = Experiment(name="test_optimizer_experiment",
+                                parameter_definitions=param_def)
+        out_queue = Queue()
+        in_queue = Queue()
+        self.backend = QueueBackend(RandomSearch, self.experiment, out_queue,
+                                    in_queue)
+
+    def test_run(self):
+        self.backend._in_queue.put("exit")
+        self.backend.run()
+
+    def test_check_update(self):
+        self.backend._check_update()
+        self.backend._in_queue.put("exit")
+        time.sleep(0.1)
+        self.backend._check_update()
+        time.sleep(0.1)
+        self.backend._check_update()
+
+        self.backend._in_queue.put(self.experiment)
+        time.sleep(0.1)
+        self.backend._check_update()
+
+    def test_check_generation(self):
+        self.backend._check_generation()
