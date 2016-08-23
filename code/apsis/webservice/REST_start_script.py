c5c2d8e15ab0cc71ad41fdb1191d8530e4d6a20e
Frederik Diehl <frederikdiehl@gmail.com>
2016-08-23 12:40:27 +0200

    Updated introduction notebook to include save_path command; more verbose initialization error when using lacking SavePath.

--- code/apsis/webservice/REST_start_script.py
+++ code/apsis/webservice/REST_start_script.py
@@ -11,6 +11,7 @@ import argparse
 
 def start_rest(save_path, port=5000, fail_deadly=False):
     print("Initialized apsis on port %s" %port)
+    print("Save_path is set to %s" %save_path)
     print("Fail_deadly is %s" %fail_deadly)
     REST_interface.start_apsis(save_path, port,
                                fail_deadly=fail_deadly)
@@ -19,7 +20,7 @@ def start_rest(save_path, port=5000, fail_deadly=False):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", help="Set the apsis server's port.")
-    parser.add_argument("save_path", help="Set a path to store logging and "
+    parser.add_argument("--save_path", help="Set a path to store logging and "
                                        "continuation information.")
     parser.add_argument("--fail_deadly", help="Fails with every exception "
                                               "instead of catching them. "
@@ -32,8 +33,9 @@ if __name__ == "__main__":
         port = args.port
     save_path = None
     fail_deadly = False
-    if args.save_path:
-        save_path = args.save_path
+    if not args.save_path:
+        raise SyntaxError("Requires a save path. Set via --save_path")
+    save_path = args.save_path
     if args.fail_deadly:
         fail_deadly = True
     start_rest(save_path, port, fail_deadly)
